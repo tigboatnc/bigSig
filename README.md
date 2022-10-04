bb7f0b50c8f556226d1886dc84d72f95e3d52059
Samarth <tigboatnc@gmail.com>
2022-10-04 16:13:23 -0400

    added first train

--- README.md
+++ README.md
@@ -6,17 +6,37 @@
 
 
 
-
+# bigSig : Machine Learning + SDR Experiments 
 
 
 
 ## Data Collection 
 
 Done using [data_collection.py](./data_collection.py) and PLUTOSDR. 
-
 __Sampling Parameters__
+
 ``` python
 sample_rate = 40e6
 center_freq = 433e6
 num_samps = 40000 
 ```
+
+## Trials and Tribulations 
+
+### m1 - Manual AWGN Noise Imputation 
+Manually adding AWGN noise in python as a substitute for a real world noisy signal 
+- [Ref](https://pysdr.org/content/noise.html)<br/>
+- [Preprocessing Notebook](./m1_preprocessing.ipynb)<br/>
+- [AutoEncoder Training Notebook](./m1_train_colab.ipynb) <br/>
+
+#### Outputs `run 1 : small poc with 190 training sample and 10 test samples`
+![op1](./assets/m1_op1.png)
+![op2](./assets/m1_op2.png)
+![op3](./assets/m1_op3.png)
+![op4](./assets/m1_op4.png)
+
+
+#### Conclusions? 
+Idk yet but 
+- the model output does make the difference between the floor and signal more apparent 
+- the model sometimes tries to create signals on the right (bias due to training as samples similar looking)
--- data_collection.py
+++ data_collection.py
@@ -1,27 +1,3 @@
-# For this coding assignment you won’t be able to use a Jupyter notebook (ipynb) because we’ll be making 
-# a real-time app, so create a regular .py file and you can run it with “python yourapp.py” in a terminal.  
-# Starting from Assignment 2 Part 3 (i.e., the spectrogram code we’ve been working on for several weeks 
-# now), if you haven’t already done so, add proper x/y axis labels to your spectrogram.  Your x-axis should 
-# have center_frequency in the center, center_frequency – sample_rate/2 on the left, and then 
-# center_frequency + sample_rate/2 on the right (if that doesn’t make any sense, email me and we can 
-# chat about what it means).  Y-axis is in time, so whether your first sample is at the bottom or top, that 
-# should start at 0, and remember that each row corresponds to fft_size * sample_period time, and 
-# sample_period is just 1/sample_rate.  For units please use milliseconds and MHz.  If you don’t know 
-# whether the first sample is at the top or bottom, just set it to a really big number and see whether the 
-# top row or bottom row gets stronger.  
-# The last task for you to do, one you finished the above, is to make your spectrogram auto-refresh, so 
-# that you can see it changing over time.  There are several ways to do this in matplotlib, one is shown 
-# below, but feel free to Google around to find other ways.  
-
-# For your submission, 
-    # set center_frequency to 100 MHz (the FM band), 
-    # sample_rate to 20 MHz,
-    #  and plug 
-# in your antenna, then take a screenshot of your spectrogram and submit it as a separate image in 
-# addition to your code.
-
-
-
 import numpy as np
 import adi
 from matplotlib import pyplot as plt
@@ -45,12 +21,7 @@ sdr.rx_buffer_size = num_samps
 
 # BIG_PICTURE = np.zeros((400,400))
 
-ds_2000 = []
-
 for i in tqdm(range(200)):
     samples = sdr.rx()
     with open(f'dataset/{int(gain)}_{i}.npy', 'wb') as f:
         np.save(f, samples)
-
-
-400,400
